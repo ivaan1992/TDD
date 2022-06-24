e9cec3fcec6537ea2c8ec9ffc30a3c69b43a7457
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-23 21:34:30 -0500

    solver class added & solver spec added

--- solver.rb
+++ solver.rb
@@ -1,13 +1,11 @@
 class Solver 
   def factorial(number)
-    a = 1
-    result = 1
-    return 'The factorials of negative integers cannot be computed!' if number.negative?
-
-    while  a < number
-        a += 1
-        result *= a
-    end
-    print 'testing'
   end
+
+  def reverse(string)
+  end
+
+  def fizzbuzz(number)
+  end
+
 end
